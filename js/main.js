7dbd2117aad1276fefdaf7b7d463c7fbd0272843
mohonish <mohonish.c@gmail.com>
2015-04-14 22:38:36 +0530

    added approach

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+
+Approach:
+> Get Data as JSON object from external file.
+> Create SVG
+> Create nodes (nested) while traversing through the json data object.
+> Position nodes in proximity based on parent-child relationship.
+> (Probably need to take care of) Collision detection and avoidance.
+> Draw lines from center of each circle, from parent to child only.
+
+Notes:
+> Depth of node in json object is tracked by d.depth method.
+
+*/
+
+$(document).ready(function(){
+
+	
+
+});
